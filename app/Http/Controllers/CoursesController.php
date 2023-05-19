f8bb29a2d782f748f140d7b409347581b7452716
dinuka9410 <93854609+dinuka9410@users.noreply.github.com>
2023-05-19 20:32:51 +0530

    commit

---
+++ app/Http/Controllers/CoursesController.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+
+class CoursesController extends Controller
+{
+    public function coursesMain()
+    {
+        addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
+
+        return view('pages.courses.courses_view');
+    }
+    public function coursesDetails()
+    {
+        addVendors(['amcharts', 'amcharts-maps', 'amcharts-stock']);
+
+        return view('pages.courses.courses_details');
+    }
+}
